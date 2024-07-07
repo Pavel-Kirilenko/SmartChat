2e649c08ad197fe755419f725cf237283e51d83f
Кириленко Павел <pavelkirilenko@mail.ru>
2024-07-07 15:25:53 +0400

    Перегрузить в классе-контейнере оператор '=' для доступа к отдельному элементу по индексу

--- MainSC.cpp
+++ MainSC.cpp
@@ -7,6 +7,7 @@ int main()
 {
 	setlocale(LC_ALL, "");
 	PersData user; // инициализация данных пользователя
-
+	PersDataArray arrayData(2);  // создание массива для храниения пользователей из десяти элементов
+	arrayData[1] = user;
 	return 0;
 }
